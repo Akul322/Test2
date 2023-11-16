854ddb9df16a714bb81e3e6279154c8e5d43cd0d
Akul <artiluckashov0000@gmail.com>
2023-11-16 15:41:41 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Interface_Final_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISum sum = new Summ();
+
+            try
+            {
+                Console.WriteLine("Write first number");
+                int a = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("You writed incorrect value!!!");
+
+            }
+
+            try
+            {
+                Console.WriteLine("Write second number");
+                int b = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("You writed incorrect value!!!");
+
+            }
+
+            sum.SumMethod(a, b);
+
+            Console.ReadLine();
+        }
+    }
+
+    public interface ISum
+    {
+        public void SumMethod(int a, int b);
+    }
+
+    public class Summ : ISum
+    {
+        void ISum.SumMethod(int a, int b)
+        {
+            int c = a + b;
+            string str = c.ToString();
+            Console.WriteLine(str);
+        }
+
+    }
+}
